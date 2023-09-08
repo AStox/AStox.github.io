d24053b359d56e39ffa57fae1751b672fa30e52a
Adam Stox <adamstox@protonmail.com>
2023-09-07 21:18:25 -0400

    update workflow

--- generate-html.js
+++ generate-html.js
@@ -1,10 +1,19 @@
 const ejs = require("ejs");
 const fs = require("fs");
+const path = require("path");
+
+// Make sure the directory exists
+const outputDir = "./public";
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
+const outputFile = path.join(outputDir, "index.html");
 
 ejs.renderFile("./views/home.ejs", {}, {}, function (err, str) {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  fs.writeFileSync("./public/index.html", str);
+  fs.writeFileSync(outputFile, str);
 });
